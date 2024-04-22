09dbc1d751e54b5549bb191c55d3a637de34f759
Andrei Huryn <gurin.nord@gmail.com>
2024-04-22 11:28:20 +0300

    fix: correct updateTaskTitleHandler

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -45,10 +45,13 @@ export const Todolist = ({
     const tasksList: JSX.Element = tasks.length === 0 ? (<p>Тасок нет</p>) : <ul>
         {tasks.map((t) => {
             const removeTaskHandler = () => removeTask(t.id, todolistId)
+
             const changeStatusHandler = (e: ChangeEvent<HTMLInputElement>) => changeTaskStatus(todolistId, t.id, e.currentTarget.checked)
-            // const updateTaskTitleHandler = (newTitle: string) => {
-            //     updateTaskTitle(todolistId, t.id, newTitle)
-            // }
+
+            const updateTaskTitleHandler = (newTitle: string) => {
+                updateTaskTitle(todolistId, t.id, newTitle)
+            }
+
             return (
                 <li key={t.id}>
                     <input
@@ -57,9 +60,10 @@ export const Todolist = ({
                         onChange={changeStatusHandler}
                     />
                     {/*<span className={t.isDone ? "task-done" : "task"}>{t.title}</span>*/}
-                    <EditableSpan oldTitle={t.title}
-                                  spanClass={t.isDone ? "task-done" : "task"}
-                                  updateTitle={(newTitle: string)=>updateTaskTitleHandler(t.id, newTitle)}
+                    <EditableSpan
+                        oldTitle={t.title}
+                        spanClass={t.isDone ? "task-done" : "task"}
+                        updateTitle={updateTaskTitleHandler}
                     />
                     {/*<Button title={"x"} onClickHandler={removeTaskHandler}/>*/}
                     <IconButton onClick={removeTaskHandler}>
@@ -100,17 +104,14 @@ export const Todolist = ({
         updateTodolistTitle(todolistId, newTitle)
     }
 
-    const updateTaskTitleHandler = (newTitle: string, taskId: string) => {
-        updateTaskTitle(todolistId, taskId, newTitle)
-    }
-
     return (
         <div className={"todolist"}>
             {/*<TodoListHeader title={title}/>*/}
             <div className={'todolist-title-container'}>
-                <EditableSpan oldTitle={title}
-                              // spanClass={t.isDone ? "task-done" : "task"}
-                              updateTitle={updateTodolistTitleHandler}
+                <EditableSpan
+                    oldTitle={title}
+                    updateTitle={updateTodolistTitleHandler}
+                    // spanClass={t.isDone ? "task-done" : "task"}
                 />
                 {/*<h3 style={{margin: 0}}>{title}</h3>*/}
                 <IconButton onClick={removeTodolistHandler}>
