a1f8be5aed625765387ba5bead69442db0925f7e
Andrei Huryn <gurin.nord@gmail.com>
2024-10-01 14:33:24 +0300

    chore: remove unnecessary comments and type AppThunk from store

--- src/app/store.ts
+++ src/app/store.ts
@@ -6,26 +6,15 @@ import { appSlice } from 'app/appSlice'
 import { authSlice } from 'features/Login/model/authSlice'
 import { configureStore, UnknownAction, combineSlices } from '@reduxjs/toolkit'
 
-// объединяя reducer-ы с помощью combineReducers,
+// объединяя reducer-ы с помощью combineReducers или combineSlices,
 // мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineSlices(tasksSlice, todolistsSlice, appSlice, authSlice)
 
 // непосредственно создаём store
-//export const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware))
-
 export const store = configureStore({ reducer: rootReducer })
 
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof store.getState>
 
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, UnknownAction>
-
 export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, UnknownAction>
 export const useAppDispatch = () => useDispatch<AppDispatch>()
-
-// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
-// @ts-ignore
-window.store = store
-
-//console.log(store.dispatch)
-//console.log(store.getState())
