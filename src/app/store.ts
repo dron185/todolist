82f5e7e903e8c8dd6789e325e2186af085ffe383
Andrei Huryn <gurin.nord@gmail.com>
2024-10-02 13:01:03 +0300

    chore: add jsdoc comment to handleServerAppError

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,10 @@
 import { tasksSlice } from 'features/TodolistsList/model/tasksSlice'
 import { todolistsSlice } from 'features/TodolistsList/model/todolistsSlice'
-import { ThunkAction, ThunkDispatch } from 'redux-thunk'
+import { ThunkDispatch } from 'redux-thunk'
 import { useDispatch } from 'react-redux'
 import { appSlice } from 'app/appSlice'
 import { authSlice } from 'features/Login/model/authSlice'
-import { configureStore, UnknownAction, combineSlices } from '@reduxjs/toolkit'
+import { combineSlices, configureStore, UnknownAction } from '@reduxjs/toolkit'
 
 // объединяя reducer-ы с помощью combineReducers или combineSlices,
 // мы задаём структуру нашего единственного объекта-состояния
