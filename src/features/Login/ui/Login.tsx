b869a64a31c2c16cbd220369df403381a244e24a
Andrei Huryn <gurin.nord@gmail.com>
2024-10-05 18:32:33 +0300

    chore: add custom hook useLogin to Login

--- src/features/Login/ui/Login.tsx
+++ src/features/Login/ui/Login.tsx
@@ -7,58 +7,15 @@ import FormGroup from '@mui/material/FormGroup'
 import FormLabel from '@mui/material/FormLabel'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
-import { useFormik } from 'formik'
-import { useAppDispatch } from 'app/store'
-import { authThunks, selectIsLoggedIn } from 'features/Login/model/authSlice'
+import { selectIsLoggedIn } from 'features/Login/model/authSlice'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
-import { BaseResponse } from 'common/types'
-
-type FormikErrorType = {
-  email?: string
-  password?: string
-  rememberMe?: boolean
-}
+import { useLogin } from 'features/Login/lib/useLogin'
 
 export const Login = () => {
-  const dispatch = useAppDispatch()
+  const { formik } = useLogin()
+
   const isLoggedIn = useSelector(selectIsLoggedIn)
-
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-      rememberMe: false,
-    },
-    validate: (values) => {
-      const errors: FormikErrorType = {}
-      if (!values.email) {
-        errors.email = 'Required'
-      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address'
-      }
-
-      if (!values.password) {
-        errors.password = 'Required'
-      } else if (values.password.length < 6) {
-        errors.password = 'Must be 6 characters or more'
-      }
-
-      return errors
-    },
-    onSubmit: (values, formikHelpers) => {
-      //alert(JSON.stringify(values, null, 2))
-      dispatch(authThunks.login(values))
-        .unwrap()
-        .then((res) => {})
-        .catch((err: BaseResponse) => {
-          err.fieldsErrors?.forEach((fieldError) => {
-            formikHelpers.setFieldError(fieldError.field, fieldError.error)
-          })
-        })
-      formik.resetForm()
-    },
-  })
 
   if (isLoggedIn) {
     return <Navigate to={'/todolists'} />
