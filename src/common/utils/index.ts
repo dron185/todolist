4cada852c4f98dd025d8561d991cacf53c63ce7c
Andrei Huryn <gurin.nord@gmail.com>
2024-10-11 21:00:41 +0300

    chore: remove thunkTryCatch, handleServerNetworkError, handleServerAppError

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,3 +1,1 @@
-export { handleServerAppError } from 'common/utils/handleServerAppError'
-export { handleServerNetworkError } from 'common/utils/handleServerNetworkError'
 export { createAppAsyncThunk } from './create-app-async-thunk'
