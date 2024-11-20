87aaddbad8f7f06aa32093f4e5292b773e439834
Andrei Huryn <gurin.nord@gmail.com>
2024-11-20 10:51:26 +0300

    chore: correct useLogin validate password

--- src/features/auth/lib/useLogin.ts
+++ src/features/auth/lib/useLogin.ts
@@ -26,8 +26,8 @@ export const useLogin = () => {
 
       if (!values.password) {
         errors.password = 'Required'
-      } else if (values.password.length < 6) {
-        errors.password = 'Must be 6 characters or more'
+      } else if (values.password.length < 4) {
+        errors.password = 'Must be 4 characters or more'
       }
 
       return errors
